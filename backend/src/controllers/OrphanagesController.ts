cc9de6207049149eaa500503cc7811f00ea5262b
Pedro Teixeira Alves <pedroteixeiraalves007@gmail.com>
2020-10-16 20:29:05 -0300

    feat(backend): uploads images from the orphanage

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -3,49 +3,55 @@ import { getRepository } from 'typeorm'
 import Orphanage from '../models/Orphanage'
 
 export default {
-    async index(request: Request, response: Response) {
-        const orphanagesRepository = getRepository(Orphanage)
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage)
 
-        const orphanages = await orphanagesRepository.find()
+    const orphanages = await orphanagesRepository.find()
 
-        return response.json(orphanages)
-    },
+    return response.json(orphanages)
+  },
 
-    async show(request: Request, response: Response) {
+  async show(request: Request, response: Response) {
+    const { id } = request.params
+    const orphanagesRepository = getRepository(Orphanage)
 
-        const { id } = request.params
-        const orphanagesRepository = getRepository(Orphanage)
+    const orphanage = await orphanagesRepository.findOneOrFail(id)
 
-        const orphanage = await orphanagesRepository.findOneOrFail(id)
+    return response.json(orphanage)
+  },
 
-        return response.json(orphanage)
-    },
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    } = request.body;
 
-    async create(request: Request, response: Response) {
-        const {
-            name,
-            latitude,
-            longitude,
-            about,
-            instructions,
-            opening_hours,
-            open_on_weekends
-        } = request.body;
-    
-        const orphanagesRepository = getRepository(Orphanage)
-    
-        const orphanage = orphanagesRepository.create({
-            name,
-            latitude,
-            longitude,
-            about,
-            instructions,
-            opening_hours,
-            open_on_weekends
-        })
-    
-        await orphanagesRepository.save(orphanage)
-    
-        return response.status(201).json(orphanage)
-    }
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const requestImages = request.files as Express.Multer.File[]
+
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images,
+    })
+
+    await orphanagesRepository.save(orphanage)
+
+    return response.status(201).json(orphanage)
+  }
 }
