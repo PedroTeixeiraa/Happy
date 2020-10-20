ba4a85a2b7ea6d6b2792c7a1e8e4ced7c1c5d1c0
Pedro Teixeira Alves <pedroteixeiraalves007@gmail.com>
2020-10-20 19:05:44 -0300

    feat(web): create routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi'
 
 import './styles/global.css'
-import './styles/pages/landing.css'
 
-import logoImg from './images/logo.svg'
+import Routes from './routes'
 
 function App() {
   return (
-    <div className="App">
-       <div className="content-wrapper">
-          <img src={logoImg} alt="logo-happy"/>
-
-          <main>
-            <h1>Leve felicidade para o mundo</h1>
-            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-          </main>
-
-          <div className="location">
-            <strong>Esteio</strong>
-            <span>Rio Grande do Sul</span>
-          </div>
-
-          <a href="" className="enter-app">
-            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-          </a>
-       </div>
-    </div>
-  );
+    <Routes />   
+  )
 }
 
-export default App;
+export default App
