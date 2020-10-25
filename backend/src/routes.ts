c0e9d02b4391d3e4b8ad2dab67a21939b2b6e92d
Pedro Teixeira Alves <pedroteixeiraalves007@gmail.com>
2020-10-25 17:57:00 -0300

    feat(api): restricted access - create user controller for registration

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,12 +3,17 @@ import multer from 'multer'
 
 import uploadConfig from './config/upload'
 import OrphanagesController from './controllers/OrphanagesController'
+import UsersController from './controllers/UsersController'
 
 const routes = Router()
 const upload = multer(uploadConfig)
 
+// ORPHANAGES
 routes.get('/orphanages', OrphanagesController.index)
 routes.get('/orphanages/:id', OrphanagesController.show)
 routes.post('/orphanages', upload.array('images'), OrphanagesController.create)
 
+// USERS
+
+routes.post('/users', UsersController.create)
 export default routes
