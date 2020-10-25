cb32eebd1ab81dec25fef59c18468deed1eb173b
Pedro Teixeira Alves <pedroteixeiraalves007@gmail.com>
2020-10-25 18:29:57 -0300

    feat(api): restricted access - password bcript when create a new user

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -82,6 +82,6 @@ export default {
 
     await orphanagesRepository.save(orphanage)
 
-    return response.status(201).json(orphanage)
+    return response.status(201).json(orphanageView.render(orphanage))
   }
 }
--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -1,28 +1,44 @@
 import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
 import * as Yup from 'yup'
+import { hash } from 'bcrypt'
+import users_view from '../views/users_view'
 
 import User from '../models/User'
 
 export default {
   async create(request: Request, response: Response) {
 
+    const {
+      name,
+      email,
+      password,
+    } = request.body
+
+    const hashedPassword = await hash(password, 8)
+
     const usersRepository = getRepository(User)
 
-     const schema = Yup.object().shape({
+    const data = {
+      name,
+      email,
+      password: hashedPassword,
+    }
+
+    const schema = Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string().required(),
       password: Yup.string().required(),
     }) 
 
-    await schema.validate(request.body, {
+    await schema.validate(data, {
       abortEarly: false
     })
-
-    const user = usersRepository.create(request.body)
-
+    
+    const user = usersRepository.create(data)
+    
     await usersRepository.save(user)
 
-    return response.status(201).json(user)
+    return response.status(201).json(users_view.render(user))
   }
 }
